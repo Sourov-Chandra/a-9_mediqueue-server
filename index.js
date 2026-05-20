59ca7d01337bef7fb7edf39002944329dba27611
sourov-chandra <sourovtpi@yahoo.com>
2026-05-20 16:51:35 +0600

    create database and api for getting tutorsCollection

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.NEXT_PUBLIC_APP_URL], 
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+        const db = client.db("mediqueue");
+        const tutorsCollection = db.collection("tutors");
+        const bookingsCollection = db.collection("bookings");
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    app.get("/", (req, res) => {
+      res.send("MediQueue server running!");
+    });
+
+
+    app.get("/tutors", async (req, res) => {
+      const result = await tutorsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+
+    
+
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
