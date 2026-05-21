927c2f8729c4887428194aeb661c162660261d51
sourov-chandra <sourovtpi@yahoo.com>
2026-05-21 22:29:57 +0600

    'updated

---
+++ middleware/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function verifyToken(req, res, next) {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
