f9ff481415bc31c5c25d7dbaac0fb15dea69edc0
sourov-chandra <sourovtpi@yahoo.com>
2026-05-21 15:02:10 +0600

    made some api like get data of my tutors data getting, updating and deleting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [process.env.NEXT_PUBLIC_APP_URL], 
+    origin: [process.env.NEXT_PUBLIC_APP_URL],
     credentials: true,
   }),
 );
@@ -25,9 +25,9 @@ async function run() {
   try {
     await client.connect();
 
-        const db = client.db("mediqueue");
-        const tutorsCollection = db.collection("tutors");
-        const bookingsCollection = db.collection("bookings");
+    const db = client.db("mediqueue");
+    const tutorsCollection = db.collection("tutors");
+    const bookingsCollection = db.collection("bookings");
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
@@ -39,12 +39,10 @@ async function run() {
       res.send("MediQueue server running!");
     });
 
-
     app.get("/tutors", async (req, res) => {
       const result = await tutorsCollection.find({}).toArray();
       res.send(result);
     });
-
 
     app.get("/tutors/:id", async (req, res) => {
       const { id } = req.params;
@@ -53,8 +51,51 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/tutors", async (req, res) => {
+      const tutor = req.body;
+      const result = await tutorsCollection.insertOne(tutor);
+      res.send(result);
+    });
+
+    app.get("/my-tutors", async (req, res) => {
+      const { email } = req.query;
+      const query = email ? { email } : {};
+      const result = await tutorsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/tutors/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await tutorsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.patch("/tutors/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedTutor = req.body;
+      const result = await tutorsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedTutor },
+      );
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
+
+      const alreadyBooked = await bookingsCollection.findOne({
+        tutorId: booking.tutorId,
+        studentEmail: booking.studentEmail,
+      });
+
+      if (alreadyBooked) {
+        return res.status(400).send({
+          message: "You have already booked this tutor session!",
+        });
+      }
+
       const tutor = await tutorsCollection.findOne({
         _id: new ObjectId(booking.tutorId),
       });
@@ -72,7 +113,6 @@ async function run() {
 
       res.send(result);
     });
-
   } finally {
   }
 }
